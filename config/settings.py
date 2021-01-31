056674b8adeb34795f7304d69c4dec53d18bb864
Bartek Mucha <muchzill4@gmail.com>
2021-01-31 16:55:20 +0000

    Move config where it belongs

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app_name.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app_name.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
