e79533124de0e59a6e2a51c26351a335f8fd2931
Bartek Mucha <muchzill4@gmail.com>
2021-01-31 21:00:54 +0000

    Timeago intergration

--- project_name/views.py
+++ project_name/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.utils.timezone import now
 
 
 def hello_world(request: HttpRequest) -> HttpResponse:
-    return render(request, "hello-world.html")
+    return render(request, "hello-world.html", {
+        "now": now(),
+    })
