96192898bfb49de8c69a459a439e2780546cf6a6
Bartek Mucha <muchzill4@gmail.com>
2021-02-12 15:58:57 +0000

    Display missing template vars in DEBUG

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "string_if_invalid": "<< MISSING VARIABLE %s >>" if DEBUG else "",
         },
     },
 ]
