20d7895682e65c2a7361485a0f2d8bac5b795690
Bartek Mucha <muchzill4@gmail.com>
2021-05-16 21:40:19 +0100

    Use tmp for less confusion

--- startproject.sh
+++ startproject.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-project_name=$1
+name=$1
+dir=$2
 
-if [ -z "$project_name" ]; then
-  echo "Usage: /bin/bash $(basename $0) project_name"
+if [ -z "$name" ]; then
+  echo "Usage: /bin/bash $(basename $0) name [directory]"
   exit 1
 fi
 
-template=${2-https://github.com/muchzill4/django-template/archive/master.zip}
+template=${3-https://github.com/muchzill4/django-template/archive/master.zip}
 additional_templates=(.env.example test-and-lint.yml docker-compose.yml)
 
 django-admin startproject \
   --template=$template \
   --name $(IFS=, ; echo "${additional_templates[*]}") \
-  $project_name
+  $name ${dir:+$dir}
